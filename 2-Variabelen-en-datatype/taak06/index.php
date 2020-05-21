44df4d9534458f1814b390a4a2508f5e7e57d739
Elyesa_Caglar <elyesa.caglar2002@gmail.com>
2020-05-21 23:57:58 +0200

    taak 6 af

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,16 @@
+<?php
+$string = "Hello";
+echo gettype($string) . "<br>";
+
+$integer = 50;
+echo gettype($integer) . "<br>";
+
+$float = 2.48;
+echo gettype($float) . "<br>";
+
+$boolean = true;
+echo gettype($boolean) . "<br>";
+
+$null = null;
+echo gettype($null) . "<br>";
+?>
