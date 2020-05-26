49114fe918b70a4955dedd82557be7bbfc7a852b
Elyesa_Caglar <elyesa.caglar2002@gmail.com>
2020-05-26 15:04:47 +0200

    3 taak 1 af

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,7 @@
+<?php
+$myMovies = array("Dragon ball super: broly", "Batman", "The Joker");
+$myMovies[0] = "Dragon ball super: broly";
+$myMovies[1] = "Batman";
+$myMovies[2] = "The Joker";  
+
+?>
