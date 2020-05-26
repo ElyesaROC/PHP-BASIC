abbaf4d67766dbc4005a3894d07202ca1502dc4a
Elyesa_Caglar <elyesa.caglar2002@gmail.com>
2020-05-26 15:09:03 +0200

    3 taak 1 is af

--- 3-Arrays/taak01/index.php
+++ 3-Arrays/taak01/index.php
@@ -1,7 +1,7 @@
 <?php
-$myMovies = array("Dragon ball super: broly", "Batman", "The Joker");
 $myMovies[0] = "Dragon ball super: broly";
 $myMovies[1] = "Batman";
 $myMovies[2] = "The Joker";  
+echo "$myMovies[0] <br> $myMovies[1] <br> $myMovies[2]"
 
 ?>
